c1a4669c2583e27f9a26f3a93b5ae51a63822810
Sabi Kumar <skumar213@gmail.com>
2022-03-01 09:59:00 -0500

    fixed user model

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -46,8 +46,8 @@ User.authenticate = async function({ username, password }){
 
 User.findByToken = async function(token) {
   try {
-    const {id} = await jwt.verify(token, process.env.JWT)
-    const user = User.findByPk(id)
+    const {id} = jwt.verify(token, process.env.JWT)
+    const user = await User.findByPk(id)
     if (!user) {
       throw 'nooo'
     }
